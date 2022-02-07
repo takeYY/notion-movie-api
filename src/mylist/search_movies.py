c4ca38c36eb408e983486cb86e2388b65bb4058f
takeyy <t0k6yist@gmail.com>
2022-02-07 15:58:02 +0900

    リファクタリング

--- src/mylist/search_movies.py
+++ src/mylist/search_movies.py
@@ -21,5 +21,4 @@ def rating_limitation(movies: pd.DataFrame, rating: list) -> pd.DataFrame:
 def genres_limitation(movies: pd.DataFrame, genres: list, and_search: bool) -> pd.DataFrame:
     tmdb_ids = [movie.tmdb_id for movie in movies.itertuples()
                 if and_or_search(movie.genres, genres, and_search)]
-    movies = movies.query(' tmdb_id in @tmdb_ids ').reset_index(drop=True)
-    return movies
+    return movies.query(' tmdb_id in @tmdb_ids ').reset_index(drop=True)
